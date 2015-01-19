e10b92ac34d38ddc03bf6d2097f7f5a5c7623ecc
Ge Gao <noreply>
2015-01-19 23:12:54 +0000

    Add component segmented-bar

---
+++ ui/segmented-bar.reel/segmented-bar.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/segmented-bar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SegmentedBar
+ * @extends Component
+ */
+exports.SegmentedBar = Component.specialize(/** @lends SegmentedBar# */ {
+    constructor: {
+        value: function SegmentedBar() {
+            this.super();
+        }
+    }
+});
